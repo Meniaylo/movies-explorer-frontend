6cfd5060f8bf47b17682f42befc06dacbccd7b40
yzma <upthere@yandex.ru>
2023-05-25 16:07:09 +0300

    add testing links

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,8 @@ export const MOVIES_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
 export const testingCard = {
   name: 'The Brave Little Toaster',
-  link: 'https://i.ytimg.com/vi/5EZpCkdaKig/maxresdefault.jpg',
-  // link: 'https://i.pinimg.com/736x/87/47/8a/87478ade9503562a7ddf7c401ecfef3b--kid-movies-disney-movies.jpg',
+  link: 'https://i.pinimg.com/736x/87/47/8a/87478ade9503562a7ddf7c401ecfef3b--kid-movies-disney-movies.jpg',
+  // link: 'https://i.ytimg.com/vi/5EZpCkdaKig/maxresdefault.jpg',
   length: '1ч 30м'
 };
 
